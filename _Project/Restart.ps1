763d11e3be4bca4a2fe2f42cba9b336f21d0e755
Simone Bembi <simone.bembi@gmail.com>
2020-07-23 01:15:28 +0200

    Updated restart script

--- _Project/Restart.ps1
+++ _Project/Restart.ps1
@@ -1,3 +1,3 @@
 npx fkill-cli Aio.Presentation.Web --force
-cd $PSScriptRoot/../Build
+cd $PSScriptRoot/../Build/current
 Start-Process ./Aio.Presentation.Web -RedirectStandardOutput '.\log.out' -RedirectStandardError '.\log.err'
